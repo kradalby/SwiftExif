ca00a67ea66fae678fc08b56e6473173d26afaf8
Alex Tran Qui <alex.tranqui@asymtech.eu>
2022-03-03 12:15:32 +0100

    - add orientation enum

--- Sources/SwiftExif/Image.swift
+++ Sources/SwiftExif/Image.swift
@@ -2,6 +2,21 @@ import Foundation
 import exif
 import iptc
 
+
+public enum Orientation: Int {
+    case normal = 1
+    case flippedH // flip H to straighten
+    case rotated180 // rotate 180 to straighten
+    case flippedV // flip V to straighten
+    case flippedHRotated270 // flip H and rotate 90 to straighten
+    case rotated90 // rotate 270 to straighten
+    case flippedHRotated90 // flip H and rotate 270 to straighten
+    case rotated270 // rotate 90 to straighten
+    case unknown
+
+}
+
+
 public struct Image {
   var exifData: ExifData?
   var iptcData: IptcData?
@@ -43,4 +58,18 @@ public struct Image {
     }
     return [:]
   }
+}
+
+extension Image {
+    var orientation: Orientation {
+        
+        if let mainImage = self.ExifRaw()["0"],
+           let orientationStr = mainImage["Orientation"],
+           let orientation = Int(orientationStr)
+        {
+            return Orientation(rawValue: orientation) ?? .unknown
+        } else {
+            return .unknown
+        }
+    }
 }
