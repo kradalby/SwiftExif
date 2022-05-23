24764cd3af5fa362ea1b3072051ccd8b040a0235
Alex Tran Qui <alex.tranqui@asymtech.eu>
2022-05-23 19:48:44 +0200

    - fix indentation to match original one.

--- Sources/SwiftExif/Image.swift
+++ Sources/SwiftExif/Image.swift
@@ -4,54 +4,54 @@ import iptc
 
 
 public enum Orientation: Int {
-    case normal = 1
-    case flippedH // flip H to straighten
-    case rotated180 // rotate 180 to straighten
-    case flippedV // flip V to straighten
-    case flippedHRotated270 // flip H and rotate 90 to straighten
-    case rotated90 // rotate 270 to straighten
-    case flippedHRotated90 // flip H and rotate 270 to straighten
-    case rotated270 // rotate 90 to straighten
-    case unknown
-
+  case normal = 1
+  case flippedH // flip H to straighten
+  case rotated180 // rotate 180 to straighten
+  case flippedV // flip V to straighten
+  case flippedHRotated270 // flip H and rotate 90 to straighten
+  case rotated90 // rotate 270 to straighten
+  case flippedHRotated90 // flip H and rotate 270 to straighten
+  case rotated270 // rotate 90 to straighten
+  case unknown
+  
 }
 
 
 public struct Image {
   var exifData: ExifData?
   var iptcData: IptcData?
-
+  
   public init(imagePath: URL) {
     exifData = ExifData.new(imagePath: imagePath.path)
     iptcData = IptcData.new(imagePath: imagePath.path)
   }
-
-    public init(_ data: Data) {
-      exifData = ExifData.new(data)
-      iptcData = IptcData.new(data)
-    }
-
+  
+  public init(_ data: Data) {
+    exifData = ExifData.new(data)
+    iptcData = IptcData.new(data)
+  }
+  
   public func Exif() -> [String: [String: String]] {
     if var data = self.exifData {
       return data.toDict()
     }
     return [:]
   }
-
+  
   public func ExifRaw() -> [String: [String: String]] {
     if var data = self.exifData {
       return data.toRawDict()
     }
     return [:]
   }
-
+  
   public func ExifWithRaw() -> [String: [String: (String, String)]] {
     if var data = self.exifData {
       return data.toValueAndRawValueDict()
     }
     return [:]
   }
-
+  
   public func Iptc() -> [String: Any] {
     if let data = self.iptcData {
       return data.toDict()
@@ -61,15 +61,15 @@ public struct Image {
 }
 
 extension Image {
-    public var orientation: Orientation {
-        
-        if let mainImage = self.ExifRaw()["0"],
-           let orientationStr = mainImage["Orientation"],
-           let orientation = Int(orientationStr)
-        {
-            return Orientation(rawValue: orientation) ?? .unknown
-        } else {
-            return .unknown
-        }
+  public var orientation: Orientation {
+    
+    if let mainImage = self.ExifRaw()["0"],
+       let orientationStr = mainImage["Orientation"],
+       let orientation = Int(orientationStr)
+    {
+      return Orientation(rawValue: orientation) ?? .unknown
+    } else {
+      return .unknown
     }
+  }
 }
--- Sources/SwiftExif/Iptc.swift
+++ Sources/SwiftExif/Iptc.swift
@@ -2,113 +2,113 @@ import Foundation
 import iptc
 
 extension IptcData {
+  
+  static func new(imagePath: String) -> IptcData? {
+    let rawUnsafeIptcData = iptc_data_new_from_jpeg(imagePath)
     
-    static func new(imagePath: String) -> IptcData? {
-        let rawUnsafeIptcData = iptc_data_new_from_jpeg(imagePath)
-        
-        if let rawIptcData = rawUnsafeIptcData {
-            return rawIptcData.pointee
+    if let rawIptcData = rawUnsafeIptcData {
+      return rawIptcData.pointee
+    }
+    
+    return nil
+  }
+  
+  
+  static func new(_ data: Data) -> IptcData? {
+    data.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
+      let rawUnsafeIptcData = iptc_data_new_from_data(bytes, UInt32(data.count))
+      
+      if let rawIptcData = rawUnsafeIptcData {
+        return rawIptcData.pointee
+      }
+      
+      return nil
+    }
+    
+  }
+  
+  func datasets() -> [IptcDataSet] {
+    if let rawDataset = self.datasets {
+      let datasets = Array(
+        UnsafeBufferPointer(
+          start: rawDataset,
+          count: Int(self.count)
+        )
+      )
+      
+      return datasets.compactMap({ $0?.pointee })
+    }
+    
+    return []
+  }
+  
+  func toTuples() -> [(String, String)] {
+    var list = [(String, String)]()
+    for var dataset in self.datasets() {
+      if let tuple = dataset.toTuple() {
+        list.append(tuple)
+      }
+    }
+    return list
+  }
+  
+  func toDict() -> [String: Any] {
+    var result = [String: Any]()
+    for (key, value) in self.toTuples() {
+      if result.keys.contains(key) {
+        if var currentValue = result[key] as? [String] {
+          currentValue.append(value)
+          result[key] = currentValue
+        } else if let currentValue = result[key] as? String {
+          result[key] = [currentValue, value]
         }
-        
-        return nil
+      } else {
+        result[key] = value
+      }
     }
     
-    
-    static func new(_ data: Data) -> IptcData? {
-        data.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) in
-            let rawUnsafeIptcData = iptc_data_new_from_data(bytes, UInt32(data.count))
-            
-            if let rawIptcData = rawUnsafeIptcData {
-                return rawIptcData.pointee
-            }
-            
-            return nil
-        }
-        
-    }
-    
-    func datasets() -> [IptcDataSet] {
-        if let rawDataset = self.datasets {
-            let datasets = Array(
-                UnsafeBufferPointer(
-                    start: rawDataset,
-                    count: Int(self.count)
-                )
-            )
-            
-            return datasets.compactMap({ $0?.pointee })
-        }
-        
-        return []
-    }
-    
-    func toTuples() -> [(String, String)] {
-        var list = [(String, String)]()
-        for var dataset in self.datasets() {
-            if let tuple = dataset.toTuple() {
-                list.append(tuple)
-            }
-        }
-        return list
-    }
-    
-    func toDict() -> [String: Any] {
-        var result = [String: Any]()
-        for (key, value) in self.toTuples() {
-            if result.keys.contains(key) {
-                if var currentValue = result[key] as? [String] {
-                    currentValue.append(value)
-                    result[key] = currentValue
-                } else if let currentValue = result[key] as? String {
-                    result[key] = [currentValue, value]
-                }
-            } else {
-                result[key] = value
-            }
-        }
-        
-        return result
-    }
-    
-    func keywords() -> [String] {
-        return self.toTuples().compactMap({ (key, value) -> String? in
-            return key == "Keywords" ? value : nil
-        })
-    }
-    
+    return result
+  }
+  
+  func keywords() -> [String] {
+    return self.toTuples().compactMap({ (key, value) -> String? in
+      return key == "Keywords" ? value : nil
+    })
+  }
+  
 }
 
 extension IptcDataSet {
-    mutating func value() -> String {
-        let value = UnsafeMutablePointer<Int8>.allocate(capacity: 256)
-        iptc_dataset_get_as_str(&self, value, 256)
-        
-        let str = String(cString: value)
-        return str
-    }
+  mutating func value() -> String {
+    let value = UnsafeMutablePointer<Int8>.allocate(capacity: 256)
+    iptc_dataset_get_as_str(&self, value, 256)
     
-    func tagTitle() -> String? {
-        let value = iptc_tag_get_title(self.record, self.tag)
-        
-        let str = String(cString: value)
-        return str
-    }
+    let str = String(cString: value)
+    return str
+  }
+  
+  func tagTitle() -> String? {
+    let value = iptc_tag_get_title(self.record, self.tag)
     
-    mutating func format() -> IptcFormat {
-        iptc_dataset_get_format(&self)
-    }
+    let str = String(cString: value)
+    return str
+  }
+  
+  mutating func format() -> IptcFormat {
+    iptc_dataset_get_format(&self)
+  }
+  
+  mutating func formatName() -> String? {
+    let value = iptc_format_get_name(self.format())
     
-    mutating func formatName() -> String? {
-        let value = iptc_format_get_name(self.format())
-        
-        let str = String(cString: value)
-        return str
+    let str = String(cString: value)
+    return str
+  }
+  
+  mutating func toTuple() -> (String, String)? {
+    if let key = self.tagTitle() {
+      return (key, self.value())
     }
-    
-    mutating func toTuple() -> (String, String)? {
-        if let key = self.tagTitle() {
-            return (key, self.value())
-        }
-        return nil
-    }
+    return nil
+  }
 }
